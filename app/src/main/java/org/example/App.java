f7f3cf328f3cf76056bcb2c6cbc736b6660a6152
HeyExotic <RubixKingdom3211@gmail.com>
2025-04-10 19:45:11 -0500

    Color Change

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,11 +10,11 @@ public class App {
         List<Integer> nums1 = List.of(1, 3, 5, 7);
         List<Integer> nums2 = List.of(2, 4, 6, 8);
         List<Integer> mergedNumbers = ListZipper.zip(nums1, nums2);
-        System.out.println(mergedNumbers); // [1, 2, 3, 4, 5, 6, 7, 8]
+        System.out.println(mergedNumbers);
         
-        List<String> colors1 = List.of("Red", "Green", "Blue");
-        List<String> colors2 = List.of("White", "Black", "Orange", "Pink", "Fuschia");
+        List<String> colors1 = List.of("Maroon", "Olive", "Cyan");
+        List<String> colors2 = List.of("Grey", "White", "Burnt Sienna", "Hot Pink", "Ultraviolet");
         List<String> mergedWords = ListZipper.zip(colors1, colors2);
-        System.out.println(mergedWords); // ["Red", "White", "Green", "Black", "Blue", "Orange", "Pink", "Fuschia"]
+        System.out.println(mergedWords);
     }
 }
